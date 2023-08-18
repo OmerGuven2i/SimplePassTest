f3559468cbbb1ff09565912bf243511e6ed19069
OmerGuven2i <omer.guven@2itesting.com>
2023-08-18 11:58:09 +0100

    Simple test where all pass

---
+++ src/test/java/JiraJenkins/simpleTest.java
@@ -0,0 +1,42 @@
+package JiraJenkins;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class simpleTest {
+
+    @Test
+    public void basicTest1(){
+        int mynumber = 6;
+        assertEquals(mynumber, 6);
+    }
+
+    @Test
+    public void basicTest2(){
+        int mynumber = 7;
+        assertEquals(mynumber, 7);
+    }
+
+    @Test
+    public void basicTest3(){
+        int mynumber = 4;
+        assertEquals(mynumber, 4);
+    }
+
+
+    @Test
+    public void basicTest4(){
+        int mynumber = 1;
+        assertEquals(mynumber, 1);
+    }
+
+    @Test
+    public void basicTest5(){
+        int mynumber = 5;
+        assertEquals(mynumber, 5);
+    }
+
+
+}
